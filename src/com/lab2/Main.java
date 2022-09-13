7badb777d20650050b72953cec13ebce83d574ed
vernik03 <vernik03@gmail.com>
2022-09-13 22:36:15 +0300

    task 2 done

--- src/com/lab2/Main.java
+++ src/com/lab2/Main.java
@@ -1,171 +1,134 @@
 package com.lab2;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Main {
-
-    int x_size = 4;
-    int y_size = 3;
-    int width = 380*x_size;
-    int height = 380*y_size;
-    boolean dead_bear = false;
-
 
     public static void main(String[] args) {
 
+        int warehouse_size = 100;
+        Munition warehouse=new Munition(warehouse_size);
+        Ivanov ivanov = new Ivanov(warehouse);
+        Petrov petrov = new Petrov(warehouse);
+        Nechiporchuk nechiporchuk = new Nechiporchuk(warehouse);
+        warehouse.info();
+        new Thread(ivanov).start();
+        new Thread(petrov).start();
+        new Thread(nechiporchuk).start();
+    }
+}
+// Класс Магазин, хранящий произведенные товары
+class Munition{
+    public int warehouse;
+    private int pile_near_warehouse=0;
+    private int truck=0;
+    private int check_list=0;
 
-        ArrayList<GUI.BeeThread> MyThreadArray = new ArrayList<>();
-        for (int i = 0; i < y_size; i++) {
-            for (int j = 0; j < x_size; j++) {
-                MyThreadArray.add(new GUI.BeeThread(j, i));
-            }
-        }
-
-
-
-
-        ArrayList<JLabel> BackArray = new ArrayList<>();
-        for (int i = 0; i < y_size; i++) {
-            for (int j = 0; j < x_size; j++) {
-                BackArray.add(new JLabel(new ImageIcon("src/com/lab2/img/back.png")));
-                grid.add(BackArray.get(BackArray.size()-1));
-                BackArray.get(BackArray.size()-1).setBounds(j*380, i*380, 380, 380);
-            }
-        }
-
-
-        for (GUI.BeeThread thread: MyThreadArray) {
-            thread.start();
-        }
-
-
+    public Munition(int warehouse){
+        this.warehouse=warehouse;
     }
 
-    public class Mutex {
-        private boolean flag = false;
-        public void mute() {
-            flag = true;
-        }
-        public void unmute() {
-            flag = false;
-        }
-        public boolean isMuted() {
-            return flag;
-        }
+    public synchronized void info() {
+        System.out.println("На складі: " + warehouse + " штук");
+        System.out.println("Вкрали зі складу: " + pile_near_warehouse + " штук");
+        System.out.println("У вантажівці: " + truck + " штук");
+        System.out.println("Порахували: " + check_list + " штук");
+        System.out.println();
     }
 
-    public class BeeThread extends Thread {
-        public boolean run_flag = true;
-        GUI.BeeThread.Textures bee_textures;
-        GUI.BeeThread.Textures textures_left;
-        GUI.BeeThread.Textures textures_right;
-        JLabel bee;
-        boolean local_kill = false;
-
-        GUI.BeeThread.Coords coords;
-        public class Coords{
-            public int x;
-            public int y;
-            public Coords(int x, int y){
-                this.x=x*380;
-                this.y=y*380;
+    public synchronized void getFromWarehouse() {
+        while (pile_near_warehouse>=10) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
             }
         }
-
-        public class Textures{
-            BufferedImage bee;
-            BufferedImage bee_50opacity;
-            BufferedImage bee_hit;
-            public Textures(String direction) {
-                try {
-                    bee= ImageIO.read(new File("src/com/lab2/img/bee_"+direction+".png"));
-                    bee_50opacity = ImageIO.read(new File("src/com/lab2/img/bee_"+direction+"_50opacity.png"));
-                    bee_hit = ImageIO.read(new File("src/com/lab2/img/bee_"+direction+"_hit.png"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        for (int i=0;i<10;i++) {
+            if (warehouse>0) {
+                warehouse--;
+                pile_near_warehouse++;
             }
         }
-
-        public BeeThread(int x, int y){
-
-            bee_textures = textures_right;
-            bee.setIcon(new ImageIcon(bee_textures.bee));
-
-            bee.setBounds(5+coords.x, 5+coords.y, 48, 45);
-
-            bee.setVisible(false);
+        info();
+        notify();
+    }
+    public synchronized void getFromPile() {
+        while (truck>=10) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
         }
+        for (int i=0;i<10;i++) {
+            if (pile_near_warehouse>0) {
+                pile_near_warehouse--;
+                truck++;
+            }
+        }
+        info();
+        notify();
+    }
 
-        public void run() {
-            while (mutex.isMuted()) {
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    public synchronized void getFromTruck() {
+        while (truck<1) {
+            try {
+                wait();
             }
-            if (!mutex.isMuted()&&!dead_bear){
-                mutex.mute();
-                int speed = 5;
-                int delay = 7;
-                try {
-                    Thread.sleep(300);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                setBee();
-                while (bee.getY() + bee.getHeight()*2 < 380+coords.y && !local_kill) {
-                    try {
-                        bee_textures = textures_right;
-                        bee.setIcon(new ImageIcon(bee_textures.bee));
-                        while (bee.getX() + bee.getWidth() < 380+coords.x) {
-                            bee.setLocation(bee.getX() + speed, bee.getY());
-                            if (killTheBear()) {
-                                returnBee();
-                                break;
-                            }
-                            Thread.sleep(delay);
-                        }
-                        for (int i = 0; i < 5; i++) {
-                            bee.setLocation(bee.getX(), bee.getY() + speed);
-                            if (killTheBear()) {
-                                returnBee();
-                                break;
-                            }
-                            Thread.sleep(delay);
-                        }
-                        bee_textures = textures_left;
-                        bee.setIcon(new ImageIcon(bee_textures.bee));
-                        while (bee.getX() > 0+coords.x) {
-                            bee.setLocation(bee.getX() - speed, bee.getY());
-                            if (killTheBear()) {
-                                returnBee();
-                                break;
-                            }
-                            Thread.sleep(delay);
-                        }
-                        for (int i = 0; i < 5; i++) {
-                            bee.setLocation(bee.getX(), bee.getY() + speed);
-                            if (killTheBear()) {
-                                returnBee();
-                                break;
-                            }
-                            Thread.sleep(delay);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                mutex.unmute();
-                returnBee();
+            catch (InterruptedException e) {
             }
+        }
+        for (int i=0;i<10;i++) {
+            if (truck>0) {
+                truck--;
+                check_list++;
+            }
+        }
+        info();
+        notify();
+    }
+}
+
+class Ivanov implements Runnable{
+
+    Munition store;
+    int warehouse_size;
+    Ivanov(Munition store){
+        this.store=store;
+        warehouse_size = store.warehouse;
+    }
+    public void run(){
+        for (int i = 1; i <= warehouse_size/10; i++) {
+            store.getFromWarehouse();
+        }
+    }
+}
+
+class Petrov implements Runnable{
+
+    Munition store;
+    int warehouse_size;
+    Petrov(Munition store){
+        this.store=store;
+        warehouse_size = store.warehouse;
+    }
+    public void run(){
+        for (int i = 1; i <= warehouse_size/10; i++) {
+            store.getFromPile();
+        }
+    }
+}
+
+class Nechiporchuk implements Runnable{
+
+    Munition store;
+    int warehouse_size;
+    Nechiporchuk(Munition store){
+        this.store=store;
+        warehouse_size = store.warehouse;
+    }
+    public void run(){
+        for (int i = 1; i <= warehouse_size/10; i++) {
+            store.getFromTruck();
         }
     }
 }
